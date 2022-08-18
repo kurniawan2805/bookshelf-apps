1c64ab5782fe2457b80b8e6449249c762c69d47c
Adi Kurniawan <42286406+kurniawan2805@users.noreply.github.com>
2022-08-18 19:04:19 +0700

    first commit bookshelf app

---
+++ main.js
@@ -0,0 +1,15 @@
+let id = new Date();
+// console.log(id);
+
+hasRead = [];
+notRead = [];
+
+hasRead.push({
+  id: +new Date(),
+  title: "Harry Potter and the Philosopher's Stone",
+  author: "J.K Rowling",
+  year: 1997,
+  isComplete: false,
+});
+
+console.log(hasRead);
